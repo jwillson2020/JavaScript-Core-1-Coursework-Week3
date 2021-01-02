36b237c0ffd716d738a23e940f6304f61b56b576
jwillson2020 <73646450+jwillson2020@users.noreply.github.com>
2021-01-02 18:15:45 +0000

    worked on D
    pairs students and mentors

--- 1-exercises/D-array-filter/exercise.js
+++ 1-exercises/D-array-filter/exercise.js
@@ -6,12 +6,26 @@
   - Do not edit any of the existing code
 */
 
+//if (!Array.isArray) {
+  //Array.isArray = function(arg) {
+   // return Object.prototype.toString.call(arg) === '[object Array]';
+  //};
+//}
+
 var pairsByIndexRaw = [[0, 3], [1, 2], [2, 1], null, [1], false, "whoops"];
 
-var pairsByIndex; // Complete this statement
+var pairsByIndex = pairsByIndexRaw.filter(elem => null !== elem && elem.length === 2); // pairs student and mentors
+
+// [0,3]  = ['Islam', 'Luke']
+
+//elem.length === 2
+//'null' != elem
+//&& elem.isArray([])
 
 var students = ["Islam", "Lesley", "Harun", "Rukmini"];
 var mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
+
+
 
 var pairs = pairsByIndex.map(function (indexes) {
   var student = students[indexes[0]];
